c518117d1b02384d0f99d0dcdf24de1522c77783
beasonshu <beasonshu@sina.com>
2019-07-23 10:23:56 +0800

    Update pdftk.rb

--- pdftk.rb
+++ pdftk.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class PkgExtract < CurlDownloadStrategy
   def stage
-    safe_system '/usr/bin/xar', '-xf', $HOME/Library/Caches/Homebrew/downloads/
+    safe_system '/usr/bin/xar', '-xf', '/Users/shuxinghu/Library/Caches/Homebrew/downloads/c94247ce6a067cee85cc03cf89c827f31b715fb26c6fe748cf9e1e2a61e667d9--pdftk_server-2.02-mac_osx-10.11-setup.pkg'
     chdir
     safe_system 'mv *.pkg/Payload Payload.gz'
     safe_system 'ls | grep -v Payload | xargs rm -r'
@@ -11,9 +11,10 @@ end
 
 class Pdftk < Formula
   homepage 'http://www.pdflabs.com/tools/pdftk-server'
-  url 'http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.6-setup.pkg',
+  url 'http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg',
     :using => PkgExtract
-  sha1 '93226ecef996d7ad833a773ee15f9b62365fb567'
+  sha256 'c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c'
+
 
 
   def install
